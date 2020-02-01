3f8ab3f6f61c7c0bcb5baaa6b108000306267f4f
LoganMazurek <lmazurek@me.com>
2020-02-01 11:38:20 -0500

    Removed Unneccessary files

--- Dino Builder/Assets/Scripts/BuildNode.cs
+++ Dino Builder/Assets/Scripts/BuildNode.cs
@@ -3,17 +3,32 @@
 public class BuildNode : MonoBehaviour
 {
     public Color hoverColor;
+
+    private GameObject turret;
+
     private Color startColor;
     private Renderer rend;
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there");//TODO: display to screen
+        }
+
+
+    }
     private void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
     }
+
     private void OnMouseEnter()
     {
         rend.material.color = hoverColor;
     }
+
     private void OnMouseExit()
     {
         rend.material.color = startColor;
