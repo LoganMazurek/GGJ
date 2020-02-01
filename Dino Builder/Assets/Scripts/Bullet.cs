0bb02a3735464ee0194277d47b321c08ac828ed6
LoganMazurek <lmazurek@me.com>
2020-02-01 14:01:52 -0500

    forgot some files from last one

---
+++ Dino Builder/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 15f;
+    public float dmg;
+    public GameObject impactEffect;
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.position += dir.normalized * distanceThisFrame;
+    }
+
+    void HitTarget()
+    {
+        target.GetComponent<Enemy>().TakeDamage(dmg);
+        GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
+    }
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+}
+
