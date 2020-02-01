d4cc688cb98f0d7b9ef12a8e6a1e670c95a42baa
LoganMazurek <lmazurek@me.com>
2020-02-01 00:35:35 -0500

    Turret Targeting and following

---
+++ Dino Builder/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WaveSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class WaveSpec
+    {
+        public int enemyType;
+        public int numEnemies;
+    };
+    public Transform enemyPrefab;
+    //add more enemyPrefabs here to give choice of enemy type
+    public Transform spawnPoint;
+    public WaveSpec[] waves;
+
+    public float timeBetweenWaves = 6f;
+    public float timeBetweenEnemies = 0.3f;
+
+    public Text waveCountdownText;
+
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+    
+    private void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waves.Length; i++)
+        {
+            for (int j = 0; j < waves[i].numEnemies; j++)
+            {
+                SpawnEnemy(enemyPrefab);
+                yield return new WaitForSeconds(timeBetweenEnemies);
+            }
+        }
+       //Debug.Log("Wave Inbound!");
+        
+    }
+
+    void SpawnEnemy(Transform enemyType)
+    {
+        Instantiate(enemyType, spawnPoint.position,spawnPoint.rotation);
+    }
+}
